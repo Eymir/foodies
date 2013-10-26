0555ba222559aa01e09c28cdc7485daa481e0e5f
cwei200pa <cwei200pa@gmail.com>
2013-10-26 02:16:45 +0000

    db test

---
+++ Foodies/src/edu/gatech/foodies/vo/Recipe.java
@@ -0,0 +1,53 @@
+package edu.gatech.foodies.vo;
+
+public class Recipe {
+	
+	private String Name;
+	private String Ingredients;
+	private int Time;
+	private int Servings;
+	private String Instruction;
+	
+	public Recipe(){}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getIngredients() {
+		return Ingredients;
+	}
+
+	public void setIngredients(String ingredients) {
+		Ingredients = ingredients;
+	}
+
+	public int getTime() {
+		return Time;
+	}
+
+	public void setTime(int time) {
+		Time = time;
+	}
+
+	public int getServings() {
+		return Servings;
+	}
+
+	public void setServings(int servings) {
+		Servings = servings;
+	}
+
+	public String getInstruction() {
+		return Instruction;
+	}
+
+	public void setInstruction(String instruction) {
+		Instruction = instruction;
+	}
+
+}
