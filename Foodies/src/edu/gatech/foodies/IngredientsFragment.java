58033bf2be2ad2fbac5ee13c4816f4d4e4fe3993
wangleshi1991 <wangleshi1991@hotmail.com>
2013-10-26 01:53:29 +0000

    for chen to test

---
+++ Foodies/src/edu/gatech/foodies/IngredientsFragment.java
@@ -0,0 +1,38 @@
+package edu.gatech.foodies;
+
+import edu.gatech.foodies.adapters.ImageAdapter;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+public class IngredientsFragment extends Fragment {
+	GridView gridView;
+	static final String[] MOBILE_OS = new String[] { "Android", "iOS",
+		"Windows", "Blackberry" };
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_main_ingredients,
+				container, false);
+		gridView = (GridView) rootView.findViewById(R.id.ingredients_gridview);
+		
+		gridView.setAdapter(new ImageAdapter(rootView.getContext(), MOBILE_OS));
+		gridView.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position,
+					long id) {
+				
+				
+			}
+			
+		});
+		
+		return rootView;
+	}
+}
